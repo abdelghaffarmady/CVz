a30a2a6b79c4d0e35dd2c23d06e68a87aac98024
abdelghaffarmady <51787411+abdelghaffarmady@users.noreply.github.com>
2023-10-17 16:08:03 +0300

    Add faker

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'vtjm9y',
   e2e: {
     baseUrl:"https://pre-cveeez.azurewebsites.net/auth/job-seeker-registration",
     setupNodeEvents(on, config) {
