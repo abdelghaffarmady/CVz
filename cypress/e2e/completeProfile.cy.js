86ead055d051f1e3da339e56ad78dc8f9c01a3a7
RehamAbdulhameed <53119319+RehamAbdulrahim@users.noreply.github.com>
2023-10-26 11:33:25 +0300

    Revert "Merge branch 'main' into Strip-Trails"
    This reverts commit c2ebf0a79d5ae8146cbd0b3f406281b0dfbb8888, reversing
    changes made to d1dccc3f46dda22e4655b0d1a87d1aefc0a8beb3.

--- cypress/e2e/completeProfile.cy.js
+++ cypress/e2e/completeProfile.cy.js
@@ -7,8 +7,15 @@ describe("Create new job post", () => {
     cy.visit("/auth/login");
     let _ = require("lodash");
 
-
-
+    /*    cy.request({
+          url: "api/app/manage-account/current-user",
+          failOnStatusCode: false,
+          method:"Get",
+          body:{
+              email: "luigi1130@wremail.xyz",
+              password: "Soforx@123" ,
+          }
+      }); */
 
     cy.get("input[formcontrolname='email']").type("jajsnsls@azaloptions.com");
     cy.get("input[formcontrolname='password']").type("Soforx@123");
@@ -99,60 +106,4 @@ describe("Create new job post", () => {
     //     );
     //   });
   });
-
-  /*   cy.get("label[id='continue-step2']",{timeout: 10000}).click()
-
-    cy.get("input[formcontrolname='birthDate']").type("1995-02-27","{enter}")
-
-    cy.get("input[id='attachementFileStorageName']").selectFile('cypress/fixtures/files/jobSeekerPhoto.png',{force:true})
-
-    cy.get("input[formcontrolname='linkedIn']").type("https://www.linkedin.com/in/abd/")
-
-    cy.get("input[id='cvAttachementFileStorageName']").selectFile("cypress/fixtures/files/jobSeekerCv.pdf",{force:true})
-
-    cy.get("input[id='vedioAttachementFileStorageName']").selectFile("cypress/fixtures/files/jobSeekerVideo.mp4",{force:true})
-    
-    //
-    cy.get('span[class="dropdown-btn"]').eq(0).click();
-    cy.get(
-        "ul.item2> :nth-child(" +
-        _.sample([1, 2, 4, 5, 6, 7, 8, 9, 10]) + ")") .eq(0).click();
-
-     cy.get('span[class="dropdown-btn"]').eq(1).click();
-     cy.contains(_.sample(["Medicine", "Accounting", "Accounting"])).click(
-            { force: true }
-          );
-
-          cy.get("select[id='inputState6']").select("Mid Career",{force:true})
-          cy.get("select[id='inputState7']").select("2-5 years",{force:true})
-
-          cy.get('span[class="dropdown-btn"]').eq(2).click();
-          cy.contains(
-            _.sample([
-              "Diploma",
-              "Bachelor's Degree",
-              "Master's Degree",
-              "Doctorate/Ph.D.",
-            ])
-          ).click({ force: true });
-         
-          cy.get('span[class="dropdown-btn"]').eq(3).click();
-          cy.contains(
-            _.sample([
-              "Communication Skills",
-              "Problem Solving",
-              "Teamwork",
-              "Customer Service",
-            ])
-          ).click();
-
-          cy.get("textarea[formcontrolname='summary']",{timeout: 60000}).type("In publishing and graphic design Lorem ipsum is a placeholder text commonly used to demonstrate  a placeholder before final copy is available")
-    cy.get("textarea[formcontrolname='summary']").type("In publishing and graphic design Lorem ipsum is a placeholder text commonly used to demonstrate  a placeholder before final copy is available")
-    cy.contains("Save").click() 
-    cy.url().should("contain","/auth/payment-package")
-  */
-    //Package Step
-    
-        
 });
-})
